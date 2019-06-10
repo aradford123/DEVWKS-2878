4dfe733a300bb990861da718cd977a7828f8c7a5
Adam Radford (aradford) <aradford@cisco.com>
2019-06-10 23:34:13 +1000

    assurnace tools

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-source env/bin/activate
+source env3/bin/activate
 jupyter nbconvert ./.notebooks/01_AssuranceAPI.ipynb --to notebook --ClearOutputPreprocessor.enabled=True --stdout > ./lab-notebooks/01_AssuranceAPI.ipynb
+jupyter nbconvert ./.notebooks/02_AssuranceAPI.ipynb --to notebook --ClearOutputPreprocessor.enabled=True --stdout > ./lab-notebooks/02_AssuranceAPI.ipynb
--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
-virtualenv  -p python2 env
-source env/bin/activate
+#virtualenv  -p python2 env
+python3 -m venv env3
+source env3/bin/activate
 
 pip install -r requirements.txt
+git clone https://github.com/CiscoDevNet/DNAC-Assurance.git
 
